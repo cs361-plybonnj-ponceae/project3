9767c4ff0576f778d301610196b14b7fe6bcfde5
nplybon <plybonnj@dukes.jmu.edu>
2022-03-01 17:12:41 -0500

    post email

--- cs361_project3/project3.c
+++ cs361_project3/project3.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <assert.h>
+#include <stdint.h>
 
 #include "common.h"
 #include "classify.h"
@@ -14,6 +15,7 @@ int main(int argc, char *argv[])
 {
     int input_fd;
     int classification_fd;
+    int map_fd;
     int cluster_number;
     int bytes_read;
     unsigned char classification, cluster_type;
@@ -34,78 +36,95 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    classification_fd = open(CLASSIFICATION_FILE, O_RDWR | O_CREAT, 0600); // Instead of opening this file here, you may elect to open it before the classifier loop in read/write mode
+    // Creates and opens a classifcation file for writing to
+    classification_fd = open(CLASSIFICATION_FILE, O_RDWR | O_CREAT, 0600); 
 
-    
-    // Iterate through all the clusters, reading their contents 
-    // into cluster_data
+    // Used to keep track of clusters in the while loops
     cluster_number = 0;
 
+    // Iterate through all the clusters, reading their contents 
+    // into cluster_data
     while ((bytes_read = read(input_fd, &cluster_data, CLUSTER_SIZE)) > 0) {
         assert(bytes_read == CLUSTER_SIZE);
         classification = TYPE_UNCLASSIFIED;
+
         /*
             In this loop, you need to implement the functionality of the
             classifier. Each cluster needs to be examined using the functions
             provided in classify.c. Then for each cluster, the attributes
             need to be written to the classification file.
         */
-        // JH JO JO HH HO HF JO JF H- > testinput
-        // 03 01 01 18 08 28 01 05 38
-
-        printf("Processing cluster %06d\n", cluster_number); // This line needs to be removed in your final submission
    
-        if (has_jpg_body(&cluster_data[cluster_number])) {
+        // Checks that the current cluster is of type JPG by looking for
+        // its body, and if it has either a header or a footer on each 
+        // iteration. This helps avoid false negatives
+        if (has_jpg_body(cluster_data)) {
             classification = TYPE_IS_JPG;
-            if (has_jpg_header(&cluster_data[cluster_number]) | has_jpg_footer(&cluster_data[cluster_number])) {
-            classification = TYPE_IS_JPG;
+            if (has_jpg_header(cluster_data) 
+                | has_jpg_footer(cluster_data)) {
+                classification = TYPE_IS_JPG;
             }
         }
-
-        if (has_html_body(&cluster_data[cluster_number])) {
+        
+        // Checks that the current cluster is of type HTML by looking for
+        // its body, and if it has either a header or a footer on each 
+        // iteration. This helps avoid false negatives
+        if (has_html_body(cluster_data)) {
             classification = TYPE_IS_HTML;
-            if (has_html_header(&cluster_data[cluster_number]) | has_html_footer(&cluster_data[cluster_number])) {
-            classification = TYPE_IS_HTML;
-            }
+            if (has_html_header(cluster_data) 
+                | has_html_footer(cluster_data)) {
+                classification = TYPE_IS_HTML;
+            }  
         } 
         
-      
-        if (has_jpg_header(&cluster_data[cluster_number])) {
+        // Check if the current cluster is a JPG header
+        // and set the file attribute to 0x03
+        if (has_jpg_header(cluster_data)) {
             classification = TYPE_IS_JPG | TYPE_JPG_HEADER;
         } 
 
-        if (has_html_header(&cluster_data[cluster_number])) {
+        // Check if the current cluster is a HTML header
+        // and set the file attribute to 0x18
+        if (has_html_header(cluster_data)) {
             classification = TYPE_IS_HTML | TYPE_HTML_HEADER;
         } 
 
-        if (has_jpg_footer(&cluster_data[cluster_number])) {
-          classification = TYPE_IS_JPG | TYPE_JPG_FOOTER;
-        }
-    
-     
-        if (has_html_footer(&cluster_data[cluster_number])) {
-          classification = TYPE_IS_HTML | TYPE_HTML_FOOTER;
+        // Check if the current cluster is a JPG footer
+        // and set the file attribute to 0x05
+        if (has_jpg_footer(cluster_data)) {
+            classification = TYPE_IS_JPG | TYPE_JPG_FOOTER;
         }
 
-        // Single HTML contains both a header and a footer followed by zero bytes
-        if (has_html_header(&cluster_data[cluster_number]) && has_html_footer(&cluster_data[cluster_number])) {
+        // Check if the current cluster is a HTML footer
+        // and set the file attribute to 0x28
+        if (has_html_footer(cluster_data)) {
+            classification = TYPE_IS_HTML | TYPE_HTML_FOOTER;
+        }
+
+        // Single HTML contains both a header and a footer followed by zero bytes.
+        // Set the file attribtue to 0x38
+        if (has_html_header(cluster_data) 
+            && has_html_footer(cluster_data)) {
             classification = TYPE_IS_HTML | TYPE_HTML_HEADER | TYPE_HTML_FOOTER;
         } 
-        // Single JPG contains both a header and a footer followed by zero bytes
-        if (has_jpg_header(&cluster_data[cluster_number]) && has_jpg_footer(&cluster_data[cluster_number])) {
+        // Single JPG contains both a header and a footer followed by zero bytes.
+        // Set the file attribute to 0x07
+        if (has_jpg_header(cluster_data) 
+            && has_jpg_footer(cluster_data)) {
             classification = TYPE_IS_JPG | TYPE_JPG_HEADER | TYPE_JPG_FOOTER;
         } 
 
-
-
-
-
-        
+        // Writes the current clusters file attribute
+        // to the classification file and move to the
+        // next cluster 
         write(classification_fd, &classification, 1);
         cluster_number++;
     }
     
     close(input_fd);
+
+    // Creates and opens a map file for writing to
+    map_fd = open(MAP_FILE, O_RDWR | O_CREAT, 0600); 
 
     // Try opening the classification file for reading, exit with appropriate
     // error message if open fails 
@@ -113,17 +132,24 @@ int main(int argc, char *argv[])
         printf("Error opening file \"%s\": %s\n", CLASSIFICATION_FILE, strerror(errno));
         return 1;
     }
-    
+
+
+   
+
     // Iterate over each cluster, reading in the cluster type attributes
     while ((bytes_read = read(classification_fd, &cluster_type, 1)) > 0) {
+
         /*
             In this loop, you need to implement the functionality of the
             mapper. For each cluster, a map entry needs to be created and
             written to the map file.
         */
+
+
+        //printf("%d\n", cluster_type);
+
+       
     }
     
     close(classification_fd);
-
-    return 0;
-};
+}
